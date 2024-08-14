f97afe9a5178eee587d26b390f09dc5be54a4a45
Mochamad Fajar Sodik <mochamad.fajarsodik@gmail.com>
2024-08-14 19:07:43 +0700

    add hero products ui

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,4 @@
-const Button = ({label, iconUrl} : {label: string, iconUrl: string}) => {
+const Button = ({ label, iconUrl }: { label: string; iconUrl: string }) => {
   return (
     <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
       {label}
