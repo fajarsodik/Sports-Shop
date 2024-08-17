b2f82012810841b580ef7e5ceb654bee69ba5b0b
Mochamad Fajar Sodik <mochamad.fajarsodik@gmail.com>
2024-08-17 12:18:52 +0700

    add superquality ui sectin

--- src/sections/SuperQuality.tsx
+++ src/sections/SuperQuality.tsx
@@ -1,7 +1,40 @@
+import Button from "../components/Button";
+import { arrowRight } from "../assets/icons";
+import { shoe8 } from "../assets/images";
+
 const SuperQuality = () => {
   return (
-    <div>SuperQuality</div>
-  )
-}
+    <section
+      id="about-us"
+      className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-containerw"
+    >
+      <div className="flex flex-1 flex-col">
+        <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          We provide you
+          <span className="text-coral-red"> Super Quality</span> Shoes
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Ensuring premium comfort and style, our meticulously crafted footwear
+          is designed to elevate your experience, providing you with unmatched
+          quality, innovation, and a touch of elegance.
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">
+          Our dedication to detail and excelence ensures your satisfaction.
+        </p>
+        <div className="mt-11"></div>
+        <Button label="View details" />
+      </div>
+      <div className="flex flex-1 justify-center items-center">
+        <img
+          src={shoe8}
+          alt="product detail"
+          width={570}
+          height={522}
+          className="object-contain"
+        />
+      </div>
+    </section>
+  );
+};
 
-export default SuperQuality
+export default SuperQuality;
